1d72e01c92d0750988f84b7bd4dca98937b1ff48
maxcramer <maximiliancramer@yahoo.com>
2022-08-15 17:01:07 +0100

    styled out the news component

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -10,18 +10,16 @@ const News = () => {
       <div className="news_signup">
         <div>
           <h1 className="signup_title">RECEIVE UPDATES</h1>
-          <h4 className="signup_tagline">
-            Stay up to date with our launch and other legal jargon
-          </h4>
+          <h4 className="signup_tagline">Stay up to date for our launch</h4>
           <input className="signup_input" placeholder="Email" type="email" />
           <button className="signup_button">
             <h5>KEEP ME UPDATED</h5>
           </button>
         </div>
       </div>
-      {/* <div>
-        <img src={onewheel} alt="Onewheel on beach" />
-      </div> */}
+      <div className="signup_image_container">
+        <img className="signup_image" src={onewheel} alt="Onewheel on beach" />
+      </div>
     </div>
   );
 };
