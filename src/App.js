4eb17eec08bcb4ae2f18360912b8f5231d5c22dc
maxcramer <maximiliancramer@yahoo.com>
2022-08-16 16:58:13 +0100

    setting up basics for registration form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./Pages/Home";
 import Footer from "./components/Footer/Footer";
-import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Register from "./Pages/Register";
 import Error from "./Pages/Error";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,7 +1,57 @@
 import React from "react";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="">First Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Last Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="">First Line of Address</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Second Line of Address</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Town</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Postcode</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Device Type</label>
+          <select name="vehicle" id="vehicle">
+            <option value="">Eskate</option>
+            <option value="">Ebike</option>
+            <option value="">EUC</option>
+            <option value="">Escooter</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">Make</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Model</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterForm;
