c8e2c2cc9ef68b1353eb01513f1f480509672f12
maxcramer <maximiliancramer@yahoo.com>
2022-08-16 15:28:17 +0100

    finally fixed scroll bar behaviou

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Home from "./Pages/Home";
 import Footer from "./components/Footer/Footer";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
   return (
     <div className="App">
-      {/* <header> */}
       <Nav />
-      {/* </header> */}
       <Home />
+      <RegisterForm />
       <Footer />
     </div>
   );
