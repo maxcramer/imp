0e268b42718c0038c0c075c8e3e9f96bc3bb6c39
maxcramer <maximiliancramer@yahoo.com>
2022-08-16 14:08:22 +0100

    nav bar hides on scroll down and reappears on scroll up

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./nav.css";
 
 const Nav = () => {
+  var lastScrollTop = 0;
+  const navbar = document.getElementById("nav");
+  window.addEventListener("scroll", function () {
+    var scrollTop =
+      window.pageYOffset || this.document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      navbar.style.top = "-70px";
+    } else {
+      navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+  });
   return (
-    <div className="nav">
+    <div className="nav" id="nav">
       <h2 className="nav_title">
         <a className="nav_title-link" href="/">
           Insure My Pev
