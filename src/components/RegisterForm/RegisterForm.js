61d01c5ba7947a1d75b1ffce49c6a36f0020daa7
maxcramer <maximiliancramer@yahoo.com>
2022-08-17 15:06:45 +0100

    styling out form, to add js next

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,55 +1,77 @@
 import React from "react";
 
+import "./registerFrom.css";
+
 const RegisterForm = () => {
   return (
     <div>
-      <form action="">
-        <div>
-          <label htmlFor="">First Name</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Last Name</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" />
-        </div>
-        <div>
-          <label htmlFor="">First Line of Address</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Second Line of Address</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Town</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Postcode</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Device Type</label>
-          <select name="vehicle" id="vehicle">
-            <option value="">Eskate</option>
-            <option value="">Ebike</option>
-            <option value="">EUC</option>
-            <option value="">Escooter</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="">Make</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Model</label>
-          <input type="text" />
-        </div>
-      </form>
+      <div className="register_form_parent">
+        <h2 className="register_form_title">
+          Register
+          <span className="register_form_underscore"></span>
+          <p className="register_tagline">
+            Sign up for the latest deals for when we launch!
+          </p>
+        </h2>
+        <form action="">
+          <div className="register_form_group">
+            <div className="form_item">
+              <label htmlFor="">First Name</label>
+              <input type="text" />
+            </div>
+            <div className="form_item">
+              <label htmlFor="">Last Name</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="form_item email">
+            <label htmlFor="">Email</label>
+            <input type="email" />
+          </div>
+          <div className="register_form_group">
+            <div className="form_item">
+              <label htmlFor="">First Line of Address</label>
+              <input type="text" />
+            </div>
+            <div className="form_item">
+              <label htmlFor="">Second Line of Address</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="register_form_group">
+            <div className="form_item">
+              <label htmlFor="">Town</label>
+              <input type="text" />
+            </div>
+            <div className="form_item">
+              <label htmlFor="">Postcode</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="register_form_group">
+            <div className="form_item">
+              <label htmlFor="">Device Type</label>
+              <select name="vehicle" id="vehicle">
+                <option value="">Eskate</option>
+                <option value="">Ebike</option>
+                <option value="">EUC</option>
+                <option value="">Escooter</option>
+              </select>
+            </div>
+            <div className="form_item">
+              <label htmlFor="">Make</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="form_item email">
+            <label htmlFor="">Model</label>
+            <input type="text" />
+          </div>
+          <button className="submit_btn">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
