6511a435bb7deb027ea37082dd2288d23806f31e
maxcramer <maximiliancramer@yahoo.com>
2022-08-17 17:32:00 +0100

    adding styling desktop and mobile to error page

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../App.css";
 
 const Error = () => {
-  return <div>Error! Page Not Found</div>;
+  let navigate = useNavigate();
+  return (
+    <div>
+      <h1 className="error_msg">Error! Page Not Found</h1>
+      <button
+        className="home_btn"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home
+      </button>
+    </div>
+  );
 };
 
 export default Error;
