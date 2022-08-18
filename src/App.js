d6bcd37e2b720e80396be6a4982ab945809bf3bd
maxcramer <maximiliancramer@yahoo.com>
2022-08-18 16:27:34 +0100

    trying to get custom 404 working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Error />} />
+          <Route path="/*" element={<Error />} />
         </Routes>
         <Footer />
       </Router>
